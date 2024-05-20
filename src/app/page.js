5535812a7173c39bb596be8dfef682281f18655d
Samir Jadhav <samirzjadhav@gmail.com>
2024-05-19 23:29:04 -0700

    feat: added anime section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Anime from "./components/Anime";
 
 export default function App() {
   return (
     <body>
       <Navbar />
       <Home />
+      <Anime />
     </body>
   );
 }
