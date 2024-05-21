c9bdee188b9af67b6c3c805e4d2d2b491c453563
Samir Jadhav <samirzjadhav@gmail.com>
2024-05-20 23:29:32 -0700

    feat:

---
+++ src/app/components/Swiper.js
@@ -0,0 +1,199 @@
+<section className="anime" id="anime">
+  <h1 className="heading">Popular on Anime</h1>
+  <div className="swiper anime-slider">
+    <div className="swiper-wrapper">
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-1.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>Naruto</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="#" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-2.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>One-Punch Man</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="#" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-3.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>DEATH NOTE</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="#" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-4.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>Black Clover</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="#" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-5.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>Jujutsu Kaisen</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="#" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-6.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>Haikyu!!</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="#" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-7.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>Kuroko's Basketball</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="#" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-8.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>My Hero Academia</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="#" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-9.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>Dr. Stone</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="#" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-10.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>Tokyo Revengers</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="#" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-11.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>Sword Art Online</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="#" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+      <div className="swiper-slide">
+        <div
+          className="box"
+          style={{ background: "url(img/anime-img-12.jpg) no-repeat" }}
+        />
+        <div className="content">
+          <h3>KENGAN ASHURA</h3>
+          <p>
+            Guided by the spirit demon within him, orphaned Naruto learns to
+            harness his powers as a ninja in this anime adventure series.
+          </p>
+          <a href="download.html" className="btn">
+            Download
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>;
