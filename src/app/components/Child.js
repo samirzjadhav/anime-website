9e80e3c3aed932b229acc88a70748c9d7c16d417
Samir Jadhav <samirzjadhav@gmail.com>
2024-05-21 20:28:05 -0700

    feat: added child section

---
+++ src/app/components/Child.js
@@ -0,0 +1,127 @@
+import React from "react";
+
+const Child = () => {
+  return (
+    <section className="child" id="child">
+      <h1 className="heading">Child Movies</h1>
+      <div className="swiper child-slider">
+        <div className="swiper-wrapper">
+          <div className="swiper-slide">
+            <div
+              className="box"
+              style={{ background: "url(img/child-1.jpg) no-repeat" }}
+            />
+            <div className="content">
+              <h3>Beyblade</h3>
+              <p>
+                Guided by the spirit demon within him, orphaned Naruto learns to
+                harness his powers as a ninja in this anime adventure series.
+              </p>
+              <a href="#" className="btn">
+                Download
+              </a>
+            </div>
+          </div>
+          <div className="swiper-slide">
+            <div
+              className="box"
+              style={{ background: "url(img/child-2.jpg) no-repeat" }}
+            />
+            <div className="content">
+              <h3>Beyblade Burst Turbo</h3>
+              <p>
+                Guided by the spirit demon within him, orphaned Naruto learns to
+                harness his powers as a ninja in this anime adventure series.
+              </p>
+              <a href="#" className="btn">
+                Download
+              </a>
+            </div>
+          </div>
+          <div className="swiper-slide">
+            <div
+              className="box"
+              style={{ background: "url(img/child-3.jpg) no-repeat" }}
+            />
+            <div className="content">
+              <h3>Bakugan: Battle Planet</h3>
+              <p>
+                Guided by the spirit demon within him, orphaned Naruto learns to
+                harness his powers as a ninja in this anime adventure series.
+              </p>
+              <a href="#" className="btn">
+                Download
+              </a>
+            </div>
+          </div>
+          <div className="swiper-slide">
+            <div
+              className="box"
+              style={{ background: "url(img/child-4.jpg) no-repeat" }}
+            />
+            <div className="content">
+              <h3>Sonic X</h3>
+              <p>
+                Guided by the spirit demon within him, orphaned Naruto learns to
+                harness his powers as a ninja in this anime adventure series.
+              </p>
+              <a href="#" className="btn">
+                Download
+              </a>
+            </div>
+          </div>
+          <div className="swiper-slide">
+            <div
+              className="box"
+              style={{ background: "url(img/child-5.jpg) no-repeat" }}
+            />
+            <div className="content">
+              <h3>Yowamushi Pedal</h3>
+              <p>
+                Guided by the spirit demon within him, orphaned Naruto learns to
+                harness his powers as a ninja in this anime adventure series.
+              </p>
+              <a href="#" className="btn">
+                Download
+              </a>
+            </div>
+          </div>
+          <div className="swiper-slide">
+            <div
+              className="box"
+              style={{ background: "url(img/child-6.jpg) no-repeat" }}
+            />
+            <div className="content">
+              <h3>Shimajiro: A Adventure</h3>
+              <p>
+                Guided by the spirit demon within him, orphaned Naruto learns to
+                harness his powers as a ninja in this anime adventure series.
+              </p>
+              <a href="#" className="btn">
+                Download
+              </a>
+            </div>
+          </div>
+          <div className="swiper-slide">
+            <div
+              className="box"
+              style={{ background: "url(img/child-7.jpg) no-repeat" }}
+            />
+            <div className="content">
+              <h3>Go Astro Boy Go!</h3>
+              <p>
+                Guided by the spirit demon within him, orphaned Naruto learns to
+                harness his powers as a ninja in this anime adventure series.
+              </p>
+              <a href="#" className="btn">
+                Download
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Child;
